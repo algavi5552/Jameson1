9b5a01445d8cc8b3ea1f277b156de49bda918521
algavi5552 <56834981+algavi5552@users.noreply.github.com>
2020-02-13 16:31:01 +0300

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ namespace Jameson
     {
         RootObject kappa;
         int ObjNumber = 1;
+        int quantity = 0;
 
         public MainWindow()
         {
@@ -20,7 +21,7 @@ namespace Jameson
         }
         private void Button_Load(object sender, RoutedEventArgs e)
         {
-            ObjNumber = Convert.ToInt32(strObjNumber.Text) - 1;//переменная для номера экземпляра класса, отображаемого на экране, начинается с 1
+            ObjNumber = Convert.ToInt32(strObjNumber.Text);//переменная для номера экземпляра класса, отображаемого на экране, начинается с 1
 
             //читаем данные из input-десериализация
             //читаем JSON из файла и десериализируем полученную из него строку
@@ -33,6 +34,7 @@ namespace Jameson
             string createText = JsonConvert.SerializeObject(kappa.D.items[ObjNumber]);
             File.WriteAllText(path, createText);
 
+            int quantity = kappa.D.items.Count;// ввели переменную для количества существующих итемов\экземпляров класса
 
             //блок вывода информации на экран
             strName.Text = kappa.D.items[ObjNumber].name;
@@ -45,26 +47,72 @@ namespace Jameson
             if (kappa.D.items[ObjNumber].phones != null)
                 for (int k = 0; k < kappa.D.items[ObjNumber].phones.Count; k++)
                 {
-                      var x = kappa.D.items[ObjNumber].phones[k];
-                    var type = x.GetType();
+                    //  var x = kappa.D.items[ObjNumber].phones[k];
+                    //var type = x.GetType();
             strPhones.Text += Convert.ToString(kappa.D.items[ObjNumber].phones[k]);//выводим лист обьектов на экран
         }
 
     }
-    private void Button_Click(object sender, RoutedEventArgs e)
-    {
-        //добавляем кнопку 'Сохранить'
+        private void Button_Dec(object sender, RoutedEventArgs e)//кнопка меняет номер читаемого элемента -1
+        {
+            strObjNumber.Text = Convert.ToString(ObjNumber--);
+
+        }
+        private void Button_Inc(object sender, RoutedEventArgs e)//кнопка меняет номер читаемого элемента +1
+        {
+            strObjNumber.Text = Convert.ToString(ObjNumber++);
+        }
+        
+            //Delete and Add есть сомнения в работе методов
+
+        private void Button_Delete(object sender, RoutedEventArgs e)//кнопка удаляет текущий экземпляр класса
+        {
+            
+            kappa.D.items.RemoveAt(ObjNumber); //удаление экземпляра чет не работает remove and dispose both
+
+            File.WriteAllText(@"output.json", JsonConvert.SerializeObject(kappa));
+        }
+        private void Button_Add(object sender, RoutedEventArgs e)//кнопка добавляет новый экземпляр класса
+        {
+            //надо добавить новый экземпляр класса?а если более 1?
+            quantity++;
+            var newItem = new Item();
+
+            newItem.name = strName.Text;
+            newItem.address = strAddress.Text;
+            newItem.type = Convert.ToInt32(strType.Text);
+            newItem.flors = Convert.ToInt32(strFlors.Text);
+            newItem.login = strLogin.Text;
+            newItem.group = Convert.ToInt32(strGroup.Text);
+
+            kappa.D.items.Add(newItem);
+
+            //kappa.D.items[quantity].name = strName.Text;
+            //kappa.D.items[quantity].address = strAddress.Text;
+            //kappa.D.items[quantity].type = Convert.ToInt32(strType.Text);
+            //kappa.D.items[quantity].flors = Convert.ToInt32(strFlors.Text);
+            //kappa.D.items[quantity].login = strLogin.Text;
+            //kappa.D.items[quantity].group = Convert.ToInt32(strGroup.Text);
+            //kappa.D.items[ObjNumber].phones = (strPhones.Text); //а как конвертировать стринг в обджект?
+
+            File.WriteAllText(@"output.json", JsonConvert.SerializeObject(kappa));
+        }
 
 
+             //Delete and Add есть сомнения в работе методов
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)//добавляем кнопку 'Сохранить'
+        {
+        
         kappa.D.items[ObjNumber].name = strName.Text;
         kappa.D.items[ObjNumber].address = strAddress.Text;
         kappa.D.items[ObjNumber].type = Convert.ToInt32(strType.Text);
         kappa.D.items[ObjNumber].flors = Convert.ToInt32(strFlors.Text);
         kappa.D.items[ObjNumber].login = strLogin.Text;
         kappa.D.items[ObjNumber].group = Convert.ToInt32(strGroup.Text);
-        //     kappa.D.items[ObjNumber].phones = (strPhones.Text); //а как конвертировать стринг в обджект?
-
-
+        //kappa.D.items[ObjNumber].phones = (strPhones.Text); //а как конвертировать стринг в обджект?
 
         File.WriteAllText(@"output.json", JsonConvert.SerializeObject(kappa));
     }
